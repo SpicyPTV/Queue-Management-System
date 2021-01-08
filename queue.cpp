a6b17f096d1d9ce6ebfe4e01bb1e6e2b69864821
Pieter Edwards <PieterEd12@Gmail.com>
2021-01-08 15:18:04 +0000

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std; 
+
+class Queue { 
+    int size; 
+    int* queue; 
+    
+    public:
+    Queue() {
+        size = 0; // Storess size
+        queue = new int[100]; // Creates Array to hold data
+    }
+    void remove() { 
+        if (size == 0) { 
+            cout << "Queue is empty"<<endl; 
+            return; 
+        } // Code for empty queue
+        else { 
+            for (int i = 0; i < size - 1; i++) { 
+                queue[i] = queue[i + 1]; 
+            } // Shifts the queue along
+            size--; // Decreases size by 1
+        } 
+    } 
+    void print() { 
+        if (size == 0) { 
+            cout << "Queue is empty"<<endl; 
+            return; 
+        } // Code for empty queue
+        for (int i = 0; i < size; i++) { 
+            cout<<queue[i]<<" <- ";
+        } // Prints queue
+        cout <<endl;
+    }
+    void add(int data) {
+        queue[size] = data; // Adds number to end of queue
+        size++; // Increases size variable
+    }
+}; 
+
+int main() { 
+    Queue q; 
+    /* Delete this comment and write your commands 
+    
+    Example:
+
+    q.add(42); q.add(2); q.add(8); q.add(1); 
+    q.remove(); 
+    q.add(128); 
+    q.print(); */
+    return 0; 
+}
